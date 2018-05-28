becc554b8d992126c39a63ef7caa59b57ef8ab9b
yinwenhui <976000584@qq.com>
2018-05-28 14:55:47 +0800

    test dev1.1

--- src/main/java/com/scau/test/Check_allowTest/Check_allowDaoTest.java
+++ src/main/java/com/scau/test/Check_allowTest/Check_allowDaoTest.java
@@ -1,6 +1,35 @@
 package com.scau.test.Check_allowTest;
 
+import com.scau.dao.*;
+import com.scau.entity.*;
 import com.scau.test.BaseTest;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Check_allowDaoTest extends BaseTest {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SalaryDao salaryDao;
+
+    @Test
+    public void findAllByPage() {
+        Map<String,Integer> map = new HashMap<String,Integer>();
+        map.put("start",0);
+        map.put("size",2);
+        List<Salary> list = salaryDao.findAllByPage(map);
+        System.out.println(list.size());
+    }
+    @Test
+    public void findAll() {
+        List<Salary> employee = salaryDao.findAll();
+        System.out.println(employee.size());
+        System.out.println(employee.get(0).toString());
+    }
 }
